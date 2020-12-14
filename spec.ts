d61ed3310923c86c5839c1ffa9be09c902766798
Ivan Berezin <berezinivan1996@gmail.com>
2020-12-14 18:10:41 +0200

    Added 'Search for specific movie' scenario + some improves

--- spec.ts
+++ spec.ts
@@ -1,22 +1,53 @@
-import {browser, $, $$} from 'protractor'
+import {browser, $, $$, ExpectedConditions as EC, element, By} from 'protractor'
 import {testData} from './testData'
 
 describe('Films', function () {
-    it('Verify top rated film',  async function () {
+
+    beforeEach(function(){
         browser.waitForAngularEnabled(false);
+    });
+
+    it('Verify top rated movie',  async function () {
         browser.get('/');
         let topRatedFilm = $$('movie-card').get(0);
     
         let nameOfTopRatedFilm = await topRatedFilm.$('.caption h4').getText();
 
         let releaseDateOfTopRatedFilm = await topRatedFilm.$('.caption h4+p').getText();
-        let onlyDate = releaseDateOfTopRatedFilm.split(' ')[2];
+        releaseDateOfTopRatedFilm = releaseDateOfTopRatedFilm.split(' ')[2];
 
         let rateOfTopRatedFilm = await topRatedFilm.$('.caption p+p small').getText();
-        console.log('rateOfTopRatedFilm: ', rateOfTopRatedFilm);
 
         expect(nameOfTopRatedFilm).toBe(testData.topRatedFilm.filmName);
-        expect(onlyDate).toBe(testData.topRatedFilm.releaseDate);
+        expect(releaseDateOfTopRatedFilm).toBe(testData.topRatedFilm.releaseDate);
         expect(rateOfTopRatedFilm).toBe(testData.topRatedFilm.filmRate);
+    });
+
+    it('Search for specific movie', async function(){
+        let inputElem = $(`.jumbotron [name='searchStr']`);
+        let searchButton = $(`.jumbotron button`);
+
+        browser.get('/');
+
+        await browser.wait(EC.visibilityOf(inputElem));
+        inputElem.sendKeys(testData.searchFilmData.filmName);
+
+        await browser.wait(EC.elementToBeClickable(searchButton));
+        searchButton.click();
+
+        await browser.wait(EC.visibilityOf(element(By.cssContainingText('.jumbotron+div h3', 'Search Results'))));
+        await browser.wait(EC.visibilityOf($('.jumbotron+div movie-card')));
+
+        let searchResultMovie = $('.jumbotron+div movie-card');
+        let searchResultMovieName = await searchResultMovie.$('.caption h4').getText();
+        
+        let searchResultMovieReleaseDate = await searchResultMovie.$('.caption h4+p').getText();
+        searchResultMovieReleaseDate = searchResultMovieReleaseDate.split(' ')[2];
+
+        let searchResultMovieRate = await searchResultMovie.$('.caption p+p small').getText();
+
+        expect(searchResultMovieName).toBe(testData.searchFilmData.filmName);
+        expect(searchResultMovieReleaseDate).toBe(testData.searchFilmData.releaseDate);
+        expect(searchResultMovieRate).toBe(testData.searchFilmData.filmRate);
     });
 });
--- testData.ts
+++ testData.ts
@@ -3,6 +3,11 @@ let testData = {
         filmName: `Gabriel's Inferno Part III`,
         releaseDate: `2020-11-19`,
         filmRate: '9.2'
+    },
+    searchFilmData: {
+        filmName: `Alien Xmas`,
+        releaseDate: `2020-11-20`,
+        filmRate: `7.2`
     }
 }
 
