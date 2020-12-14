12365dee1c5489710030e526f9b2871a963d1362
Ivan Berezin <berezinivan1996@gmail.com>
2020-12-14 16:43:37 +0200

    Added 'Verify top rated film' scenario

--- spec.ts
+++ spec.ts
@@ -1,5 +1,22 @@
-describe('Protractor', function () {
-    it('Should be alive', function () {
-        expect(true).toBe(true)
-    })
-}) 
+import {browser, $, $$} from 'protractor'
+import {testData} from './testData'
+
+describe('Films', function () {
+    it('Verify top rated film',  async function () {
+        browser.waitForAngularEnabled(false);
+        browser.get('/');
+        let topRatedFilm = $$('movie-card').get(0);
+    
+        let nameOfTopRatedFilm = await topRatedFilm.$('.caption h4').getText();
+
+        let releaseDateOfTopRatedFilm = await topRatedFilm.$('.caption h4+p').getText();
+        let onlyDate = releaseDateOfTopRatedFilm.split(' ')[2];
+
+        let rateOfTopRatedFilm = await topRatedFilm.$('.caption p+p small').getText();
+        console.log('rateOfTopRatedFilm: ', rateOfTopRatedFilm);
+
+        expect(nameOfTopRatedFilm).toBe(testData.topRatedFilm.filmName);
+        expect(onlyDate).toBe(testData.topRatedFilm.releaseDate);
+        expect(rateOfTopRatedFilm).toBe(testData.topRatedFilm.filmRate);
+    });
+});
---
+++ testData.ts
@@ -0,0 +1,9 @@
+let testData = {
+    topRatedFilm: {
+        filmName: `Gabriel's Inferno Part III`,
+        releaseDate: `2020-11-19`,
+        filmRate: '9.2'
+    }
+}
+
+export {testData};
